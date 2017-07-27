b79c393fd5070d426e22739793008e6dc1165715
framartin <martin.gubri@framasoft.org>
2017-07-26 20:50:36 -0400

    fix typo + take into account path modification in b651f88

--- scraping/generate_data.py
+++ scraping/generate_data.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
 """
-This script parses the html files inside html/xssed/ and html/randomwalk as
-defined respectively in xssed.json and randomwalk.json. It outputs data.csv.
-HTML files referenced on randomwalk.json file that cannot be found will be
-discarted (because some duplicates can be removed). 
+This script parses the html files inside html/xssed/full and 
+html/randomwalk/subsample as defined respectively in xssed.json and 
+randomwalk.json. It outputs data.csv.
+HTML files referenced on the json files that cannot be found will be discarted
+(because some duplicated or very large files can be removed). 
 """ 
 
 import json, csv
@@ -23,7 +24,7 @@ def parse_html(filename):
     """ Parses filename and returns a dict of features for future model uses"""
     try:
         with open(filename, 'r'):
-            
+            pass # TODO
     except FileNotFoundError as e:
         print("File not found. Skipping file: %s" % filename)
         return None
@@ -43,7 +44,8 @@ def main():
     data_rw = import_json('randomwalk.json')
     for page in data_rw:
         feature_class = {'class': 0} # benign
-        features_html = parse_html(page['file_path'])
+        features_html = parse_html(page['file_path'].replace(
+            'html/randomsample/', 'html/randomsample/subsample/'))
         if features_html is None: # file not found, do not write
             continue
         features_url = parse_url(page['url'])
@@ -52,7 +54,7 @@ def main():
         data.append(features_page)
     data_xssed = import_json('xssed.json')
     for page in data_xssed:
-        if page['category'] != 'XSS':
+        if page['category'] not in ['XSS', 'Script Insertion']:
             print('''Warning: non-XSS vuln imported. please check if it should
             be removed: %s''' % page['url'])
         feature_class = {'class': 1} # xss
