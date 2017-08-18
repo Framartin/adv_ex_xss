ce213a8a1f5a364d63c640ed4b09c02a57127d78
framartin <martin.gubri@framasoft.org>
2017-08-18 19:30:32 -0300

    various draft cleanups

--- adv_decision_tree.R
+++ adv_decision_tree.R
@@ -1,25 +1,14 @@
-require(partykit)
-require(data.tree)
+library(Rweka)
+library(partykit)
+library(data.tree)
 
-prty <- as.party(m1)
-tree <- as.Node(prty)
-partytree <- prty
-data.tree <- tree
-newdata <- data[1,]
+# TODO:
+# - don't forget to work only on data.pred[data.pred$class==TRUE,]
+# - warning: dependance between variables: for example, adding a new tag, increases html_length
 
-
-constraints <- vector(mode='list', length=ncol(data))
-names(constraints) <- colnames(data)
-# TRUE: contraint to an increase. derivative should be >=0
-# to apply a negaive constraint, just transform your variable to the opposite
-# becareful, logical variable are converted to integer
-constraints[['url_special_characters']] = TRUE # 0->1 ok ; 1->0 no
-# constraint all variables, except: js_file (? -> pas à contraindre, on suppose que l'algo est le seul web firewall), url_* (?)
-for (i in colnames(data)) {
-        constraints[[i]] = TRUE
-}
 
 predicted_class_leaves <- function(partytree) {
+    # DEPRECATED
     # compute the predicted class for each terminal nodes
     # usage: pred_terminal_nodes['49]
     terminal_table <- as.matrix(do.call("table", fitted(partytree)))
@@ -27,80 +16,6 @@ predicted_class_leaves <- function(partytree) {
     names(pred_terminal_nodes) <- rownames(test)
     return(pred_terminal_nodes)
 }
-
-find_adversarial_node <- function(partytree, data.tree, data, constraints = NULL) {
-    var_names <- colnames(data)
-    adv_ex <- data
-    legitimate_class <- TRUE #predict(partytree, data) # NO! We only want true-positive to be detected as false positive
-    legit_leaf <- predict(partytree, data, type = "node")
-    current_node <- legit_leaf
-    components <- c()
-    #pred_leaves <- predicted_class_leaves(partytree)
-    
-    while(predict(partytree, adv_ex) == legitimate_class) {
-        sibling <- FindNode(data.tree, current_node)$siblings[[1]] # sibling data.tree node # TODO: J48 can have more than 2 siblings
-        sibling_name <- sibling$name
-        parent <- sibling$parent
-        parent_name <- parent$name
-        condition_sibling_full = partykit:::.list.rules.party(partytree, i = sibling_name)
-        condition_parent_full = partykit:::.list.rules.party(partytree, i = parent_name)
-        if (condition_parent_full == "") { # children of the root
-            condition_sibling <- condition_sibling_full
-        } else {
-            condition_sibling = gsub(condition_parent_full, '', condition_sibling_full, fixed = TRUE)
-        }
-        condition_sibling = sub('[[:space:]]*&[[:space:]]*', '', condition_sibling)
-        var_name <- regmatches(condition_sibling, regexpr('([a-zA-Z0-9_-.]+)', condition_sibling))
-        if (! var_name %in% var_names)
-            stop('error extracting variable name')
-        if (! is.null(constraints[[var_name]])) { # if constraint on the current split variable
-            assign(var_name, data[,var_name])
-            condition_sibling_bool = eval(parse(text=condition_sibling)) # scared?
-            if (! condition_sibling_bool) { # the condition is not already fullfield
-                break_numeric <- as.numeric(regmatches(condition_sibling, regexpr('([0-9.]+$)', condition_sibling)))
-                if (grepl('<', condition_sibling, fixed=T)){ # condition is < or <=
-                    # constraint is not fullfield, ie. current value is > break_num
-                    # Not possible
-                    #next
-                } else if (grepl('>=', condition_sibling, fixed=T)) {
-                    # Possible
-                    # Set variable to break_num
-                    if (is.integer(adv_ex[,var_name])) {
-                        adv_ex[,var_name] <- as.integer(break_numeric)
-                        append(components, list(var=var_name, value=as.integer(break_numeric)))
-                    } else {
-                        stop('var type not supported')
-                    }
-                } else if (grepl('>', condition_sibling, fixed=T)) {
-                    if (is.integer(adv_ex[,var_name])) {
-                        adv_ex[,var_name] <- as.integer(break_numeric + 1)
-                        append(components, list(var=var_name, value=as.integer(break_numeric)))
-                    } else {
-                        stop('var type not supported')
-                    }
-                } else {
-                    stop('error parsing condition, operator not supported')
-                }
-                
-            }
-        }
-        append(components, 1)
-    }
-    return(legit_leaf)
-}
-
-find_adversarial_node(partytree = b, data.tree=tree , data = data.train[1,])
-
-
-exhaustive_search <- function(partytree, data.tree, data, constraints = NULL) {
-    legitimate_class <- predict(partytree, data)
-    legit_leaf <- predict(partytree, data, type = "node")
-    leaves <- nodeids(partytree, terminal = TRUE)
-    
-}
-
-# don't forget to work only on data.pred[data.pred$class==TRUE,]
-# warning: dependance between variables: for example, adding a new tag, increases html_length
 
 extract_slit_conditon <- function(node_name, partytree, data.tree) {
     # extract the condition associated with the split of the node
@@ -279,59 +194,82 @@ find_adversarial_example <- function(partytree, data.tree, newdata, legitimate_c
     return(x_adv)
 }
 
+
+# Test runs
+
+data <- read.csv('data.csv')
+# convert factor to logical when appropriate
+for (var in colnames(data)) {
+    if (is.factor(data[,var])) {
+        if (all(levels(data[,var]) %in% c("False", "True"))) {
+            #data[,var] = as.logical(data[,var])
+            data[,var] = as.integer(as.logical(data[,var])) # until the fix for the bug in logical variable handling in partykit, converts to int
+        }
+    }
+}
+
+data$class <- as.factor(as.logical(data$class))
+str(data)
+
+set.seed(42)
+n.train <- floor(nrow(data)*0.8)
+is.train <- sample(c(rep(TRUE, n.train), rep(FALSE, nrow(data) - n.train)))
+data.train <- data[is.train,]
+data.pred <- data[!is.train,]
+
+# options for J48
+WOW(J48)
+# -N <number of folds>
+# ...
+
+m1 <- J48(class~., data = data.train)
+m1
+summary(m1)
+table(data.pred$class, predict(m1, data.pred))
+prop.table(table(data.pred$class, predict(m1, data.pred)))*100
+sum(diag(prop.table(table(data.pred$class, predict(m1, data.pred)))))
+if(require("party", quietly = TRUE)) plot(m1)
+plot(m1, type="simple")
+
+
+prty <- as.party(m1)
+tree <- as.Node(prty)
+partytree <- prty
+data.tree <- tree
+newdata <- data[1,]
+
+
+constraints <- vector(mode='list', length=ncol(data))
+names(constraints) <- colnames(data)
+# TRUE: contraint to an increase. derivative should be >=0
+# to apply a negaive constraint, just transform your variable to the opposite
+# becareful, logical variable are converted to integer
+constraints[['url_special_characters']] = TRUE # 0->1 ok ; 1->0 no
+# constraint all variables, except: js_file (? -> pas à contraindre, on suppose que l'algo est le seul web firewall), url_* (?)
+for (i in colnames(data)) {
+    constraints[[i]] = TRUE
+}
+# TODO: variables:
+# - url_number_keywords
+# - js_method_alert
+
+# TODO: impact of perturbations on:
+# - url_length
+# - html_length
+
 find_adversarial_example(partytree, data.tree, newdata, constraints = constraints)
 
 data.pred.malicious <- data.pred[data.pred$class==TRUE,]
+successful_bypasses <- data.frame()
 for (i in 1:nrow(data.pred.malicious)) {
-    print(i)
-    print(find_adversarial_example(partytree, data.tree, newdata = data.pred.malicious[i, ], constraints = constraints))
+    cat('*')
+    newdata_adv <- find_adversarial_example(partytree, data.tree, newdata = data.pred.malicious[i, ], constraints = constraints)
+    if (! is.null(newdata_adv))
+        successful_bypasses <- rbind(successful_bypasses, newdata_adv)
 }
-        
-        parent <- siblings[[1]]$parent
-        parent_name <- parent$name
-        condition_sibling_full = partykit:::.list.rules.party(partytree, i = sibling_name)
-        condition_parent_full = partykit:::.list.rules.party(partytree, i = parent_name)
-        if (condition_parent_full == "") { # children of the root
-            condition_sibling <- condition_sibling_full
-        } else {
-            condition_sibling = gsub(condition_parent_full, '', condition_sibling_full, fixed = TRUE)
-        }
-        condition_sibling = sub('[[:space:]]*&[[:space:]]*', '', condition_sibling)
-        var_name <- regmatches(condition_sibling, regexpr('([a-zA-Z0-9_-.]+)', condition_sibling))
-        if (! var_name %in% var_names)
-            stop('error extracting variable name')
-        if (! is.null(constraints[[var_name]])) { # if constraint on the current split variable
-            assign(var_name, data[,var_name])
-            condition_sibling_bool = eval(parse(text=condition_sibling)) # scared?
-            if (! condition_sibling_bool) { # the condition is not already fullfield
-                break_numeric <- as.numeric(regmatches(condition_sibling, regexpr('([0-9.]+$)', condition_sibling)))
-                if (grepl('<', condition_sibling, fixed=T)){ # condition is < or <=
-                    # constraint is not fullfield, ie. current value is > break_num
-                    # Not possible
-                    #next
-                } else if (grepl('>=', condition_sibling, fixed=T)) {
-                    # Possible
-                    # Set variable to break_num
-                    if (is.integer(adv_ex[,var_name])) {
-                        adv_ex[,var_name] <- as.integer(break_numeric)
-                        append(components, list(var=var_name, value=as.integer(break_numeric)))
-                    } else {
-                        stop('var type not supported')
-                    }
-                } else if (grepl('>', condition_sibling, fixed=T)) {
-                    if (is.integer(adv_ex[,var_name])) {
-                        adv_ex[,var_name] <- as.integer(break_numeric + 1L)
-                        append(components, list(var=var_name, value=as.integer(break_numeric)))
-                    } else {
-                        stop('var type not supported')
-                    }
-                } else {
-                    stop('error parsing condition, operator not supported')
-                }
-                
-            }
-        }
-        append(components, 1)
-    }
-}
+
+# number of bypasses
+nrow(successful_bypasses)
+# % of escape
+nrow(successful_bypasses)/nrow(data.pred.malicious)
 
